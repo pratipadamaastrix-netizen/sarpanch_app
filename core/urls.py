8ce11a6b4abe57023fe665dea959e50ea1d10994
Pratipada Behera <pratipada.maastrix@gmail.com>
2026-04-17 19:03:29 +0530

    css issues

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -21,6 +23,5 @@ urlpatterns = [
     path("advertisements/", include("advertisements.urls")),
 ]
 
-# Media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ✅ REMOVE DEBUG CONDITION
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
